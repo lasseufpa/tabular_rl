d83c313faebf8aec84d8440095b23c7f3fc54a81
Aldebaro Klautau <aldebaro@ufpa.br>
2024-03-21 12:07:29 -0300

    added find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Known dynamics FMDP',
     version='1.0',
-    scripts=['src/finite_mdp_utils.py', 'src/known_dynamics_env.py'],
+    packages=find_packages()
 )
