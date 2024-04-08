b17062bda8b929b182079c2092bfcca079aaf33a
CaioBBrasil <caioberbrasil@gmail.com>
2024-04-08 08:01:05 -0300

    modifications

---
+++ tabular_rl/__init__.py
@@ -0,0 +1,12 @@
+import tabular_rl.src.finite_mdp_utils as finite_mdp_utils
+import tabular_rl.src.known_dynamics_env as known_dynamics_env
+import tabular_rl.src.mobility_utils as mobility_utils
+import tabular_rl.src.optimum_values as optimum_values
+import tabular_rl.src.verbose_kd_env as verbose_kd_envr
+
+import tabular_rl.envs.multiband_scheduling_env as multiband_scheduling
+import tabular_rl.envs.random_known_dynamics_env as random_known_dynamics
+import tabular_rl.envs.recycle_robot_env as sutton_recycle_robot
+import tabular_rl.envs.simple_known_dynamics_env as simple_known_dynamics
+import tabular_rl.envs.sutton_grid_world_env as sutton_grid_world
+import tabular_rl.envs.user_scheduling_env as user_scheduling 
